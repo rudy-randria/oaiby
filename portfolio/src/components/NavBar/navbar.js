df61699d1631e84339e16c801b62eb4496c7c006
rudy_85 <rudrandria@gmail.com>
2023-12-07 11:28:59 +0300

    my portfolio react

---
+++ portfolio/src/components/NavBar/navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './navbar.css';
+import logo from '../../assets/logo.png';
+
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <img src={logo} alt='logo'className='logo'/>
+        <div className='desktopMenu'>
+          
+        </div>
+        <button className='desktopMenuBtn'>
+            <img src='' alt='' className='desktopMenuImage' /> Contact me
+        </button>
+    </nav>
+  )
+}
+
+export default Navbar
